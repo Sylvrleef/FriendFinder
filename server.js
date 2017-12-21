1d24e90d7d1fbdea4492c6f0ae9160746b22846a
Sylvrleef <sylvrleef@gmail.com>
2017-12-21 17:18:28 -0600

    stuff

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
-const parser = require("body-parser");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+  console.log("App listening on port: " + PORT);
+});
